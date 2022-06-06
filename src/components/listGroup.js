c7af9d23fccebb36c8d10c1560ec221a66c7ea12
AnjanaPathirana99 <80908562+AnjanaPathirana99@users.noreply.github.com>
2022-06-07 00:00:32 +0530

    fix ListGroup export bug

--- src/components/listGroup.js
+++ src/components/listGroup.js
@@ -5,19 +5,19 @@ const ListGroup = (props) => {
 
     return (
         <ul className='list-group'>
-            { items.map(item => 
-            <li 
-            onClick={() => onItemSelect(item) } 
-            key={item[valueProperty]}  
-            className={item === selectedItem ? "list-group-item active" : "list-group-item"}>
-                  {item[textProperty]}</li>)}
+            {items.map(item =>
+                <li
+                    onClick={() => onItemSelect(item)}
+                    key={item[valueProperty]}
+                    className={item === selectedItem ? "list-group-item active" : "list-group-item"}>
+                    {item[textProperty]}</li>)}
 
-</ul>
+        </ul>
     );
-    }
+}
 
-    ListGroup.defaultProps = {
-        textProperty: "name",
-        valueProperty: "_id"
-    }
+ListGroup.defaultProps = {
+    textProperty: "name",
+    valueProperty: "_id"
+}
 export default ListGroup;
